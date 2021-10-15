a8720f37bf23fba7becd5c2ff1aa29cfc677efc1
Jim <36181330+eivydas-r@users.noreply.github.com>
2021-10-15 12:01:29 -0500

    formik functioning

--- src/Components/Step2/Step2.js
+++ src/Components/Step2/Step2.js
@@ -26,6 +26,8 @@ function Step2(props) {
         updatePhone(input2);
     }
 
+    
+
     const formik = useFormik({
         initialValues: {
             email: '',
@@ -45,41 +47,50 @@ function Step2(props) {
             } else if(!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(values.email)){
                 errors.email = 'Invalid email format'
             }
+            // 
             if(!values.phone){
-                errors.phone = 'Required'
+                errors.phone = 'Phone is required'
+            } else if(!/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(values.phone)){
+                errors.phone = 'Invalid phone number'
             }
-            console.log('Form values', formik.values);
+            console.log('Form errors', errors);
             return errors;
         }});
         // Formik not functioning for now
+        console.log('Form values', formik.values);
 
     return (
         // Step 2: Enter email, phone number
+        //defaultValue={store.getState().updateData.email}
+        //  defaultValue={store.getState().updateData.phoneNumber}
         <Screen>
-                <div className="main">
-                    <h1 style={{marginLeft:"0px"}}>Step 2:</h1>
-                    <form className="form" onSubmit={formik.handleSubmit}>
-                        <label style={{fontSize: "20px"}}>
-                            Email: 
-                            <input style={{marginLeft: "10px"}} type="text" name="email" ref={emailInput} defaultValue={store.getState().updateData.email}/>
-                        </label>
-                        <label style={{fontSize: "20px"}}>
-                            Phone: 
-                            <input style={{marginLeft: "10px"}} type="text" name="phone" ref={phoneInput} defaultValue={store.getState().updateData.phoneNumber}/>
-                        </label>
-                        <br/>
-                        <Link to="/steps/1">
-                            <input onClick={handleInput} className="btn btn-success" style={{fontSize: "20px", marginTop: "10px", backgroundColor:"gray", marginRight:"10px"}} type="submit" value="Back"/>
-                        </Link>
-                        <Link to="/steps/3">
-                            <input onClick={handleInput} className="btn btn-success" style={{fontSize: "20px", marginTop: "10px"}} type="submit" value="Next"/>
-                        </Link>
-                    </form>
-                </div>
-                <Link to="/">
-                    <button className="btn btn-primary" style={{marginTop:'25px', fontSize: '20px'}}>Back to start</button>
-                </Link>
-            </Screen>
+            <div className="main">
+                <h1 style={{marginLeft:"0px"}}>Step 2:</h1>
+                <form className="form" onSubmit={formik.handleSubmit}>
+                    <label htmlFor="email" style={{fontSize: "20px"}}> 
+                        Email: 
+                        <input type="email" style={{marginLeft: "10px"}} id="email" name="email" ref={emailInput}  value={formik.values.email} onChange={formik.handleChange}/>
+                    </label>
+                    {formik.errors.email ? <div className="error">{formik.errors.email}</div> : null}
+ 
+                    <label htmlFor="phone" style={{fontSize: "20px"}}>
+                        Phone: 
+                        <input style={{marginLeft: "10px"}} type="text" id="phone" name="phone" ref={phoneInput} value={formik.values.phone} onChange={formik.handleChange}/>
+                    </label>
+                    {formik.errors.phone ? <div className="error" id="bottom">{formik.errors.phone}</div> : null}
+                    <br/>
+                    <Link to="/steps/1">
+                        <input onClick={handleInput} className="btn btn-success" style={{fontSize: "20px", marginTop: "10px", backgroundColor:"gray", marginRight:"10px"}} type="submit" value="Back"/>
+                    </Link>
+                    <Link to="/steps/3">
+                        <input onClick={handleInput} className="btn btn-success" style={{fontSize: "20px", marginTop: "10px"}} type="submit" value="Next"/>
+                    </Link>
+                </form>
+            </div>
+            <Link to="/">
+                <button className="btn btn-primary" style={{marginTop:'25px', fontSize: '20px'}}>Back to start</button>
+            </Link>
+        </Screen>
     );
 }
 export default Step2;
